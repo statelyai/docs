67e6f3d371b26039df21b1002e17db101a72495e
laurakalbag <me@laurakalbag.com>
2023-03-27 14:33:40 +0100

    Improve HTML and styles of generated category index pages

---
+++ src/theme/DocCardList/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import clsx from 'clsx';
+import {
+  useCurrentSidebarCategory,
+  filterDocCardListItems,
+} from '@docusaurus/theme-common';
+import DocCard from '@theme/DocCard';
+function DocCardListForCurrentSidebarCategory({className}) {
+  const category = useCurrentSidebarCategory();
+  return <DocCardList items={category.items} className={className} />;
+}
+export default function DocCardList(props) {
+  const {items, className} = props;
+  if (!items) {
+    return <DocCardListForCurrentSidebarCategory {...props} />;
+  }
+  const filteredItems = filterDocCardListItems(items);
+  return (
+    <ul className={clsx('card-list index-list content-boxes', className)}>
+      {filteredItems.map((item, index) => (
+        <li key={index} className="card-list-item">
+          <DocCard item={item} />
+        </li>
+      ))}
+    </ul>
+  );
+}
