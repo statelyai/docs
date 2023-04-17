7310139ccc95a3a427899b0f3c2393117334d858
laurakalbag <me@laurakalbag.com>
2023-04-17 17:33:44 +0100

    Make screenshots slightly smaller

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        viewport: { width: 1920, height: 1080 },
+        viewport: { width: 1440, height: 900 },
         deviceScaleFactor: 2,
       },
     },
