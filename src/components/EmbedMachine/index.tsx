03701eddd2cf0d0d9d642db79917a954c2eac71d
laurakalbag <me@laurakalbag.com>
2023-04-27 16:59:18 +0100

    Create EmbedMachine component and embed first machine

---
+++ src/components/EmbedMachine/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+type Embed = {
+  name: string;
+  embedURL: string;
+};
+
+export default function EmbedMachine({name, embedURL}: Embed) {
+  return (
+    <p>
+        <iframe
+            name={name}
+            loading="lazy"
+            src={embedURL}
+            className={styles.embed}
+        >
+            <a href={embedURL}>View the <em>{name}</em> machine in Stately Studio</a>.
+        </iframe>
+    </p>
+  );
+}
