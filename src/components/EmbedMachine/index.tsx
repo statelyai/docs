845388ac69b8f460a72b6caf097aa4ff6fe810a4
Farzad Yousefzadeh <farskid@gmail.com>
2023-05-09 20:13:43 +0300

    Adds embed component that follows the site color mode

--- src/components/EmbedMachine/index.tsx
+++ src/components/EmbedMachine/index.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css';
+import { useColorMode } from '@docusaurus/theme-common';
 
 type Embed = {
   name: string;
   embedURL: string;
 };
 
-export default function EmbedMachine({name, embedURL}: Embed) {
+export default function EmbedMachine({ name, embedURL }: Embed) {
+  const { colorMode } = useColorMode();
   return (
-    <p>
-        <iframe
-            loading="lazy"
-            src={embedURL}
-            className={styles.embed}
-        >
-            <a href={embedURL}>View the <em>{name}</em> machine in Stately Studio</a>.
-        </iframe>
-    </p>
+    <iframe
+      loading="lazy"
+      src={manageURL(embedURL, {
+        colorMode,
+      })}
+      className={styles.embed}
+    >
+      <a href={embedURL}>
+        View the <em>{name}</em> machine in Stately Studio
+      </a>
+      .
+    </iframe>
   );
+}
+
+function manageURL(
+  embedURL: string,
+  options?: { colorMode: 'light' | 'dark' },
+): string {
+  const url = new URL(embedURL);
+  for (const opt in options) {
+    url.searchParams.set(opt, options[opt]);
+  }
+  return url.toString();
 }
