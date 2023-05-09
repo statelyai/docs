bf65cf3a529b1829a4887abcdf2475f0f6873278
Farzad Yousefzadeh <farskid@gmail.com>
2023-05-09 20:16:14 +0300

    Remove unused code

--- src/components/EmbedMachine/index.tsx
+++ src/components/EmbedMachine/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import styles from './styles.module.css';
 import { useColorMode } from '@docusaurus/theme-common';
 
