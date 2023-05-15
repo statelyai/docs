4070f31fdc4f236d0e38b05d906b2f18494da3f1
laurakalbag <me@laurakalbag.com>
2023-05-15 14:19:16 +0100

    Add and update version history screenshots (not all done yet)

--- create-screenshots/screenshots.spec.ts
+++ create-screenshots/screenshots.spec.ts
@@ -7,8 +7,13 @@ test('version-history--versions', ({ page }, { title }) =>
   takeScreenshot({
     title,
     page,
-    url: 'editor/29baf751-0b06-4c9f-a8ac-1fd470769a79?machineId=9999c2c1-c63f-4fa5-b333-b4a14dfacc3f', // version history feature machine
+    url: 'editor/4649a4f4-3624-452f-b9c2-afb51fd3f655?machineId=93df6cea-c491-4118-ae30-5e15b4d2697d', // version history feature machine
     actions: async(page: Page)=>{
+      // for full screen screenshots, make the browser a good size to capture the whole viewport without losing details
+      await page.setViewportSize({
+        width: 1200,
+        height: 675,
+      });
       // target the button with the exact name “Version history” as there are multiple buttons containing the same text
       await page.getByRole('button', { name: 'Version history', exact: true }).click()
       // wait a few seconds to ensure the panel is loaded
@@ -24,8 +29,13 @@ test('version-history--preview-version', ({ page }, { title }) =>
   takeScreenshot({
     title,
     page,
-    url: 'editor/29baf751-0b06-4c9f-a8ac-1fd470769a79?machineId=9999c2c1-c63f-4fa5-b333-b4a14dfacc3f', // version history feature machine
+    url: 'editor/4649a4f4-3624-452f-b9c2-afb51fd3f655?machineId=93df6cea-c491-4118-ae30-5e15b4d2697d', // version history feature machine
     actions: async(page: Page)=>{
+      // for full screen screenshots, make the browser a good size to capture the whole viewport without losing details
+      await page.setViewportSize({
+        width: 1200,
+        height: 675,
+      });
       // target the button with the exact name “Version history” as there are multiple buttons containing the same text
       await page.getByRole('button', { name: 'Version history', exact: true }).click()
       // wait a few seconds to ensure the panel is loaded
@@ -37,28 +47,53 @@ test('version-history--preview-version', ({ page }, { title }) =>
     }
   }));
 
-// This doesn’t work as we can’t target the restore button, but keeping because the clip options are right!
-// test('version-history--restore-version', ({ page }, { title }) =>
-//   takeScreenshot({
-//     title,
-//     page,
-//     url: 'editor/29baf751-0b06-4c9f-a8ac-1fd470769a79?machineId=9999c2c1-c63f-4fa5-b333-b4a14dfacc3f', // version history feature machine
-//     options: {
-//       clip: {
-//         width: 900,
-//         height: 520,
-//         x: 540,
-//         y: 80
-//       }
-//     },
-//     actions: async(page: Page)=>{
-//       // target the button with the exact name “Version history” as there are multiple buttons containing the same text
-//       await page.getByRole('button', { name: 'Version history', exact: true }).click()
-//       // wait a few seconds to ensure the panel is loaded
-//       await page.waitForTimeout(3000)
-//       // click the 2nd version button in the version history panel
-//       await page.getByTestId('version-select-btn').nth(2).hover();
-//       // wait a few seconds to ensure the preview is loaded
-//       await page.waitForTimeout(3000)
-//     }
-//   }));
+// has annoying tooltip visible in screenshot
+test('version-history--restore-version', ({ page }, { title }) =>
+  takeScreenshot({
+    title,
+    page,
+    url: 'editor/4649a4f4-3624-452f-b9c2-afb51fd3f655?machineId=93df6cea-c491-4118-ae30-5e15b4d2697d', // version history feature machine
+    options: {
+      clip: {
+        width: 900,
+        height: 520,
+        x: 540,
+        y: 80
+      }
+    },
+    actions: async(page: Page)=>{
+      // target the button with the exact name “Version history” as there are multiple buttons containing the same text
+      await page.getByRole('button', { name: 'Version history', exact: true }).click()
+      // wait a few seconds to ensure the panel is loaded
+      await page.waitForTimeout(3000)
+      // click the 2nd version button in the version history panel
+      await page.getByTestId('version-select-btn').nth(1).focus();
+      // wait a few seconds to ensure the preview is loaded
+      await page.waitForTimeout(3000)
+    }
+  }));
+
+test('version-history--rename-version', ({ page }, { title }) =>
+  takeScreenshot({
+    title,
+    page,
+    url: 'editor/4649a4f4-3624-452f-b9c2-afb51fd3f655?machineId=93df6cea-c491-4118-ae30-5e15b4d2697d', // version history feature machine
+    options: {
+      clip: {
+        width: 900,
+        height: 520,
+        x: 540,
+        y: 80
+      }
+    },
+    actions: async(page: Page)=>{
+      // target the button with the exact name “Version history” as there are multiple buttons containing the same text
+      await page.getByRole('button', { name: 'Version history', exact: true }).click()
+      // wait a few seconds to ensure the panel is loaded
+      await page.waitForTimeout(3000)
+      // click the version name input box and fill in “New name”
+      await page.getByLabel('Version name').nth(1).fill('New name');
+      // wait a few seconds to ensure the preview is loaded
+      await page.waitForTimeout(3000)
+    }
+  }));
