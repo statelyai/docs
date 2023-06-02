f7c8b98fae3b9da6754565c0168f56551034b863
Anders Bech Mellson <mellson@icloud.com>
2023-06-02 10:02:56 +0200

    Fix broken links and port Announcement component

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -2,6 +2,7 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import ThemedImage from '@theme/ThemedImage';
 import * as LucideIcons from 'lucide-react';
+import { Announcement } from '../components/Announcement';
 import EmbedMachine from '../components/EmbedMachine';
 import SkipDownLink from '../components/SkipDownLink';
 import Tweet from '../components/embeds/Tweet';
@@ -10,6 +11,7 @@ import YouTube from '../components/embeds/YouTube';
 export default {
   ...MDXComponents,
   ...LucideIcons,
+  Announcement,
   SkipDownLink,
   Tweet,
   YouTube,
