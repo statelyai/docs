2ecb5d079e13994b2a7354007cd1a26c78f06e02
laurakalbag <me@laurakalbag.com>
2023-06-09 16:46:30 +0100

    Add new default og images for landing page, docs, and blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,6 +89,7 @@ const config = {
           blogDescription: 'Stately’s engineering blog',
           blogSidebarCount: 0,
           postsPerPage: 'ALL',
+          image: 'static/blog-default.png',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -100,6 +101,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: 'static/docs-default.png',
       navbar: {
         title: '',
         logo: {
