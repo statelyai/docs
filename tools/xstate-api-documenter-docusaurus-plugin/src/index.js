5bf077cfe913b1d21c360353ba4d1560acf1459f
Eric Skogen <audionerd@users.noreply.github.com>
2023-10-22 21:43:32 -0500

    docs

--- tools/xstate-api-documenter-docusaurus-plugin/src/index.js
+++ tools/xstate-api-documenter-docusaurus-plugin/src/index.js
@@ -1,4 +1,8 @@
-// Based on https://github.com/microsoft/rushstack-websites/tree/5335470/tools/api-documenter-docusaurus-plugin
+// XState-specific plugin to format API Documenter output for Docusaurus
+//
+// Adapted from https://github.com/microsoft/rushstack-websites/tree/5335470/tools/api-documenter-docusaurus-plugin
+//
+// API Documenter Plugin API reference: https://api.rushstack.io/pages/api-documenter
 
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
