f1a49a3a759f51ea3ac240055396ccc46aee9345
laurakalbag <me@laurakalbag.com>
2023-11-14 12:19:00 +0000

    Replace example image with new UI and room readings machine

--- src/components/landing/Intro.tsx
+++ src/components/landing/Intro.tsx
@@ -20,8 +20,8 @@ export function Intro() {
 
         <div className="shadow-md rounded-md w-full">
           <img
-            alt="TODO"
-            src="/landing/example-default.png"
+            alt="This state machine is called Accumulate room readings. Its purpose is to get temperature and humidity readings from IoT sensors and generate a report every hour. The state machine starts in the ConsumeReadings state, where it initializes the temperature and humidity values as null. It then waits for temperature and humidity events to be logged. When a temperature event is logged, the state machine updates the temperature value. Similarly, when a humidity event is logged, it updates the humidity value. After one hour, the state machine transitions to the GenerateReport state. In this state, it invokes a service called produceReport to generate the report. Once the report is generated, the state machine transitions back to the ConsumeReadings state to continue accumulating readings."
+            src="/landing/room-readings.png"
             className="w-full my-24 rounded-md border-[0.5px] shadow-2xl shadow-blue-900 border-blue-850"
           />
         </div>
