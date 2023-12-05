146c54982c6671c98348181915e2e57e37d4f867
Laura Kalbag <me@laurakalbag.com>
2023-12-05 17:43:20 +0000

    Use custom admonitions instead of v4 banner on v4 docs (#312)
    * Replace V4 banner with custom deprecation admonitions for each page
    
    * Add admonitions to every XState v4 page directing to relevant v5 docs
    
    * Fix broken urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
             4: {
               label: 'XState v4',
               path: 'xstate-v4',
-              banner: 'unmaintained',
+              banner: 'none',
             },
           },
 
