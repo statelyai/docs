c7311900958af537f71126723a94c9174b69a372
David Khourshid <davidkpiano@gmail.com>
2023-12-28 09:51:23 -0500

    Add Immer guide and guides section

--- sidebars.js
+++ sidebars.js
@@ -343,9 +343,19 @@ const sidebars = {
       items: ['actors', 'invoke', 'spawn', 'system', 'inspection'],
     },
     {
-      type: 'doc',
-      label: 'Testing',
-      id: 'testing',
+      type: 'category',
+      label: 'Guides',
+      link: {
+        type: 'generated-index',
+        title: 'Guides',
+        description:
+          'These guides cover different aspects such as using XState with immer, testing strategies, and other upcoming guides that will be added in the future. The purpose of these guides is to provide users with in-depth knowledge and practical examples to effectively utilize XState in their projects.',
+        slug: '/category/guides',
+        keywords: ['guides'],
+      },
+      collapsed: false,
+      collapsible: true,
+      items: ['testing', 'immer'],
     },
     {
       type: 'category',
