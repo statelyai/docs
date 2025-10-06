2f0f6b0b37568aee303d9fcd4b50f157ed97e2b5
David Khourshid <david.khourshid@tango.us>
2025-10-05 23:49:20 -0400

    Meta

--- fumadocs/lib/layout.shared.tsx
+++ fumadocs/lib/layout.shared.tsx
@@ -20,7 +20,7 @@ export function baseOptions(): BaseLayoutProps {
           >
             <circle cx={12} cy={12} r={12} fill="currentColor" />
           </svg>
-          My App
+          Stately.ai
         </>
       ),
     },
