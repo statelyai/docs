24cd67ab12d587771b9d30da4099d7f76cc3203a
David Khourshid <davidkpiano@gmail.com>
2025-12-22 09:26:40 -0500

    Move fumadocs to root

---
+++ .source/source.config.mjs
@@ -0,0 +1,50 @@
+// source.config.ts
+import {
+  defineCollections,
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+  metaSchema
+} from "fumadocs-mdx/config";
+import { transformerTwoslash } from "fumadocs-twoslash";
+import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
+import z from "zod";
+var docs = defineDocs({
+  docs: {
+    schema: frontmatterSchema,
+    postprocess: {
+      includeProcessedMarkdown: true
+    }
+  },
+  meta: {
+    schema: metaSchema
+  }
+});
+var blogPosts = defineCollections({
+  type: "doc",
+  dir: "content/blog",
+  schema: frontmatterSchema.extend({
+    authors: z.array(z.string()),
+    date: z.iso.date().or(z.date())
+  })
+});
+var source_config_default = defineConfig({
+  mdxOptions: {
+    // MDX options
+    rehypeCodeOptions: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark"
+      },
+      transformers: [
+        ...rehypeCodeDefaultOptions.transformers ?? [],
+        transformerTwoslash()
+      ]
+    }
+  }
+});
+export {
+  blogPosts,
+  source_config_default as default,
+  docs
+};
--- src/components/embeds/YouTube.tsx
+++
@@ -1,9 +0,0 @@
-import { YouTube as MDXYouTube } from 'mdx-embed';
-
-export default function YouTube({ id }: { id: string }) {
-  return (
-    <div style={{ margin: '20px 0' }}>
-      <MDXYouTube youTubeId={id} />
-    </div>
-  );
-}
