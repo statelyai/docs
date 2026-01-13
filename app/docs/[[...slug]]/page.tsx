d4f35d5c7f52329184378ccd564901ff3654d3b3
David Khourshid <davidkpiano@gmail.com>
2026-01-13 17:08:55 -0500

    Patch

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -33,7 +33,7 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
         <MDX
           components={getMDXComponents({
             // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
+            a: createRelativeLink(source as any, page),
           })}
         />
       </DocsBody>
